41eb3e34410711d46b25bc33498e57d1f6157276
Ubuntu <ubuntu@ip-172-31-2-111.ap-south-1.compute.internal>
2023-07-03 09:39:39 +0000

    preventing ansible to not expose sensitive info into logs

--- sql_file1.sql
+++ sql_file1.sql
@@ -1,5 +1,7 @@
-INSERT INTO [test_db].[dbo].[namelist] VALUES ('MS','Dhoni');
+SELECT * FROM [test_db].[dbo].[namelist];
 
-DELETE FROM [test_db].[dbo].[namelist] WHERE (firstname='James');
+DELETE FROM [test_db].[dbo].[namelist];
+
+INSERT INTO [test_db].[dbo].[namelist] VALUES ('MS','Dhoni');
 
 SELECT * FROM [test_db].[dbo].[namelist];
