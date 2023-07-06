5a63615b6398aa8c95c0a7d379f4b06eefeac9bb
Ubuntu <ubuntu@ip-172-31-2-111.ap-south-1.compute.internal>
2023-07-06 17:20:03 +0000

    adding gocd_echo.sh | gocd.yaml dryrun validation

---
+++ gocd_echo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "## Current working dir:$(pwd) ##"
+echo "## Print GOCD env variables ##"
+echo "${Playbook_name}\n${Archive_flag}\n${File_loc}\n${Tmp_loc}\n${Target_loc}\n${File_size}"
