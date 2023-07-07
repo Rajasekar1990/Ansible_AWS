673f5f8ecbfa3c7c07c9922434eb05e12b397f8c
Ubuntu <ubuntu@ip-172-31-2-111.ap-south-1.compute.internal>
2023-07-07 08:58:17 +0000

    gocd.yaml updated

--- gocd_echo.sh
+++ gocd_echo.sh
@@ -2,4 +2,4 @@
 
 echo "## Current working dir:$(pwd) ##"
 echo "## Print GOCD env variables ##"
-echo -e "Playbook_name: ${Playbook_name}\nArchive_flag: ${Archive_flag}\nFile_loc: ${File_loc}\nTmp_loc: ${Tmp_loc}\nTarget_loc: ${Target_loc}\nFile_size: ${File_size}"
+echo "Playbook_name: ${Playbook_name}\nArchive_flag: ${Archive_flag}\nFile_loc: ${File_loc}\nTmp_loc: ${Tmp_loc}\nTarget_loc: ${Target_loc}\nFile_size: ${File_size}"
