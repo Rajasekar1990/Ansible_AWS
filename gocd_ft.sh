e810de71a3e8814111d878da74ec32e037021029
Ubuntu <ubuntu@ip-172-31-2-111.ap-south-1.compute.internal>
2023-07-07 08:18:45 +0000

    gocd.yaml | gocd_ft.sh updated

--- gocd_ft.sh
+++ gocd_ft.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "## Run File Transfer Ansible-playbook ##"
-echo 'Password@123'| sudo --stdin ansible-playbook -i inventory_gocd ${Playbook_name} -vv
+sudo ansible-playbook -i inventory_gocd ${Playbook_name} -vv
 echo "## Playbook execution completed ##"
 echo "## print the list of files in the current dir"
 ls -lrat
