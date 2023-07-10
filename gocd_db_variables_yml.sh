3d15e8172dc39e38b45d749faf23008806fd0879
Ubuntu <ubuntu@ip-172-31-2-111.ap-south-1.compute.internal>
2023-07-10 08:05:29 +0000

    gocd_db_variables_yml.sh gocd_ft_variables_yml.sh updated | enclosed vars with quotes

--- gocd_db_variables_yml.sh
+++ gocd_db_variables_yml.sh
@@ -15,13 +15,13 @@ echo 'Updating variables.yml with runtime inputs provided in gocd.yaml envrionme
 escaped_sql_srcpath=$(echo "$sql_srcpath" | sed 's/\//\\\//g')
 escaped_sql_dirpath=$(echo "$sql_dirpath" | sed 's/\//\\\//g')
 
-variables_yml_sedoutput=$(cat variables_bkp.yml | sed -e "s/mssql_login_host: \(.*\)/mssql_login_host: $mssql_login_host/g" \
-                                                  -e "s/mssql_login_user: \(.*\)/mssql_login_user: $mssql_login_user/g" \
-                                                  -e "s/mssql_db: \(.*\)/mssql_db: $mssql_db/g" \
-                                                  -e "s/mssql_port: \(.*\)/mssql_port: $mssql_port/g" \
-                                                  -e "s/sql_srcpath: \(.*\)/sql_srcpath: $escaped_sql_srcpath/g" \
-                                                  -e "s/sql_dirpath: \(.*\)/sql_dirpath: $escaped_sql_dirpath/g" \
-                                                  -e "s/mssql_login_password: \(.*\)\([\\t\\r\\n\\s\\S\]*\)/mssql_login_password: $mssql_login_password/g")
+variables_yml_sedoutput=$(cat variables_bkp.yml | sed -e "s/mssql_login_host: \(.*\)/mssql_login_host: '$mssql_login_host'/g" \
+                                                  -e "s/mssql_login_user: \(.*\)/mssql_login_user: '$mssql_login_user'/g" \
+                                                  -e "s/mssql_db: \(.*\)/mssql_db: '$mssql_db'/g" \
+                                                  -e "s/mssql_port: \(.*\)/mssql_port: '$mssql_port'/g" \
+                                                  -e "s/sql_srcpath: \(.*\)/sql_srcpath: '$escaped_sql_srcpath'/g" \
+                                                  -e "s/sql_dirpath: \(.*\)/sql_dirpath: '$escaped_sql_dirpath'/g" \
+                                                  -e "s/mssql_login_password: \(.*\)\([\\t\\r\\n\\s\\S\]*\)/mssql_login_password: '$mssql_login_password'/g")
 echo "$variables_yml_sedoutput" > variables.yml
 
 
--- gocd_ft_variables_yml.sh
+++ gocd_ft_variables_yml.sh
@@ -14,14 +14,14 @@ escaped_File_loc=$(echo "$File_loc" | sed 's/\//\\\//g')
 escaped_Tmp_loc=$(echo "$Tmp_loc" | sed 's/\//\\\//g')
 escaped_Target_loc=$(echo "$Target_loc" | sed 's/\//\\\//g')
 
-variables_yml_sedoutput=$(cat variables_bkp.yml | sed -e "s/archive_flag: \(.*\)/archive_flag: $Archive_flag/g" \
-                                                  -e "s/file_loc: \(.*\)/file_loc: $escaped_File_loc/g" \
-                                                  -e "s/tmp_loc: \(.*\)/tmp_loc: $escaped_Tmp_loc/g" \
-                                                  -e "s/target_loc: \(.*\)/target_loc: $escaped_Target_loc/g" \
-                                                  -e "s/file_size: \(.*\)/file_size: $File_size/g")
+variables_yml_sedoutput=$(cat variables_bkp.yml | sed -e "s/archive_flag: \(.*\)/archive_flag: '$Archive_flag'/g" \
+                                                  -e "s/file_loc: \(.*\)/file_loc: '$escaped_File_loc'/g" \
+                                                  -e "s/tmp_loc: \(.*\)/tmp_loc: '$escaped_Tmp_loc'/g" \
+                                                  -e "s/target_loc: \(.*\)/target_loc: '$escaped_Target_loc'/g" \
+                                                  -e "s/file_size: \(.*\)/file_size: '$File_size'/g")
 echo "$variables_yml_sedoutput" > variables.yml
 
-ansible_cfg_sedoutput=$(cat ansible_bkp.cfg | sed -e "s/log_path=\(.*\)/log_path=$Ansible_logpath/g" \
-                                              -e "s/vault_password_file=\(.*\)/vault_password_file=$Vault_password_file/g")
+ansible_cfg_sedoutput=$(cat ansible_bkp.cfg | sed -e "s/log_path=\(.*\)/log_path='$Ansible_logpath'/g" \
+                                              -e "s/vault_password_file=\(.*\)/vault_password_file='$Vault_password_file'/g")
 
 echo "$ansible_cfg_sedoutput" > ansible.cfg
